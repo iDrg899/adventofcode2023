8d14f6102922e705c83836f53ebbe99564214891
iDrg899 <omnom899@gmail.com>
2023-12-09 07:07:03 -0500

    day9

---
+++ day9/part1.py
@@ -0,0 +1,25 @@
+txt = [i.strip() for i in open('day9/input.txt').readlines()]
+
+_sum = 0
+
+for l in txt:
+    nums = [int(i) for i in l.split()]
+
+    depth = 0
+    seqs = [nums]
+    current = seqs[depth]
+
+    while not all(n == 0 for n in current):
+        seqs.append([current[i + 1] - current[i] for i in range(len(current) - 1)])
+        depth += 1
+
+        current = seqs[depth]
+
+    subsum = sum([seq[-1] for seq in seqs])
+    _sum += subsum
+    
+    for i in seqs:
+        print(i)
+    print(subsum)
+
+print(_sum)
